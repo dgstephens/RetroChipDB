91bae25c8553a03f815b51ab55606973ee1893d4
Daniel Stephens <dgstephens@gmail.com>
2021-02-14 17:23:45 +0000

    Add retro_vars.php

---
+++ retro_vars.php
@@ -0,0 +1,22 @@
+<?php
+/*********************************************
+ * VARIABLE DEFINITIONS
+ *********************************************/
+
+// url
+$myx_url = "http://www.retrochipdb.com/";
+
+// tape price
+$tape_price = "15.00";
+
+// setup database connection
+$servername = "localhost";
+$username = "retrochip";
+$password = "lzv6s1m";
+$dbname = "retrodb";
+
+if( !isset( $write_to_database ))
+{
+    $write_to_database = 0;
+}
+?>
